2566c9a7dbde234d351bef2ec86891681d9a2b9d
lunaGatica <89168675+lunaGatica@users.noreply.github.com>
2023-12-04 23:20:45 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import NavBar from "./Proyecto/NavBar";
+
+const App = () => {
+    return(
+        <div style={{width: '100vw', height: '100vh'}}>
+            <NavBar/>
+        </div>
+    );
+}
+export default App;
